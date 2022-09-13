01b51bf047360d06e6f46ac5bfae9b34dfbf1310
shiva148 <shivakumarvishwakarma1@gmail.com>
2022-09-13 11:21:40 +0530

    Add Mvc to Application

---
+++ MyBookStore/MyBookStore/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace MyBookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "MyBook";
+        }
+    }
+}
--- MyBookStore/MyBookStore/Startup.cs
+++ MyBookStore/MyBookStore/Startup.cs
@@ -16,6 +16,9 @@ namespace MyBookStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddMvc();
+            //services.AddControllers();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +33,7 @@ namespace MyBookStore
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
